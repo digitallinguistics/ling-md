56eb213edd0ec4adfd57d793c4a3bc7e1930fdec
Daniel W. Hieber <dwhieb@gmail.com>
2024-08-15 22:19:28 -0500

    DEV: Set up tests (#2)
    closes #2

---
+++ index.test.js
@@ -0,0 +1,10 @@
+import { expect }       from 'chai'
+import { describe, it } from 'node:test'
+
+describe(`Tests`, function() {
+
+  it(`should pass`, function() {
+    expect(true).to.equal(true)
+  })
+
+})
